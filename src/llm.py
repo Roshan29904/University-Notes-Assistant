3dc9b11f0e24e9d1645344bb064f411b3998a009
Roshan29904 <roshan.rai29904@gmail.com>
2026-07-12 04:26:30 +0530

    llm changed done

--- src/llm.py
+++ src/llm.py
@@ -7,8 +7,9 @@ load_dotenv()
 
 def get_llm():
     llm = HuggingFaceEndpoint(
-        repo_id="meta-llama/Llama-3.1-8B-Instruct",
+        repo_id=os.getenv("HF_CHAT_MODEL"),
         task="text-generation",
+        provider=os.getenv("HF_INFERENCE_PROVIDER"),
         huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
         temperature = 0.65,
         max_new_tokens = 1024,
